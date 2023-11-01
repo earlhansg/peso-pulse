3aaaafb2ec9a2c523ea3982581cc34942f2e7f22
Earl Hans Genoso <earlhansg@gmail.com>
2023-11-01 15:43:57 +0800

    added removeCharacter method for filtering selected character

--- background.js
+++ background.js
@@ -5,3 +5,16 @@ chrome.contextMenus.create({
     title: "Add to my expenses",
     contexts: ["selection"]
 });
+
+let removeCharacter = (selectedString) => {
+    return selectedString.replace(/[^0-9]/g, '');
+}
+
+chrome.contextMenus.onClicked.addListener((clickData) => {
+    console.log("clickData", clickData)
+    if(clickData.menuItemId === "PesoPulse" && clickData.selectionText) {
+        if(removeCharacter(clickData.selectionText)) {
+            console.log("theres is a number");
+        }
+    }
+})
