1937e7293b757b6482bd487596ab33fd157ee136
Earl Hans Genoso <earlhansg@gmail.com>
2023-11-02 07:10:27 +0800

    added modal in popup user interface(initial setup)

--- background.js
+++ background.js
@@ -1,20 +1,22 @@
-console.log("background page ...")
+console.log("background page ...");
 
 chrome.contextMenus.create({
-    id: "PesoPulse",
-    title: "Add to my expenses",
-    contexts: ["selection"]
+  id: "PesoPulse",
+  title: "Add to my expenses",
+  contexts: ["selection"],
 });
 
 let removeCharacter = (selectedString) => {
-    return selectedString.replace(/[^0-9]/g, '');
-}
+  return selectedString.replace(/[^0-9]/g, "");
+};
 
-chrome.contextMenus.onClicked.addListener((clickData) => {
-    console.log("clickData", clickData)
-    if(clickData.menuItemId === "PesoPulse" && clickData.selectionText) {
-        if(removeCharacter(clickData.selectionText)) {
-            console.log("theres is a number");
-        }
+chrome.contextMenus.onClicked.addListener((clickData, tab) => {
+  console.log("clickData", clickData, tab);
+  if (clickData.menuItemId === "PesoPulse" && clickData.selectionText) {
+    if (removeCharacter(clickData.selectionText)) {
+      chrome.tabs.create({ url: "popup.html" }, (tab) => {
+        console.log("Created a new tab with ID: " + tab.id);
+      });
     }
-})
+  }
+});
--- popup.js
+++ popup.js
@@ -2,4 +2,16 @@ $(document).ready(function () {
   $("p").click(function () {
     $(this).hide();
   });
+  $("#openModal").click(function () {
+    $("#myModal").fadeIn();
+  });
+
+  $(".close").click(function () {
+    $("#myModal").fadeOut();
+  });
+  $(window).click(function (event) {
+    if (event.target == $("#myModal")[0]) {
+      $("#myModal").css("display", "none");
+    }
+  });
 });
