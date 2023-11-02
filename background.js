8bbd4c622ba14d62268a904f9e4dd4f85a79f25b
Earl Hans Genoso <earlhansg@gmail.com>
2023-11-02 08:21:08 +0800

    dynamically show modal in specific tab

--- background.js
+++ background.js
@@ -14,8 +14,10 @@ chrome.contextMenus.onClicked.addListener((clickData, tab) => {
   console.log("clickData", clickData, tab);
   if (clickData.menuItemId === "PesoPulse" && clickData.selectionText) {
     if (removeCharacter(clickData.selectionText)) {
+      chrome.storage.sync.set({'newSelectionPrice': clickData.selectionText})
       chrome.tabs.create({ url: "popup.html" }, (tab) => {
         console.log("Created a new tab with ID: " + tab.id);
+        chrome.storage.sync.set({'tabSelectionId': tab.id})
       });
     }
   }
--- popup.js
+++ popup.js
@@ -1,4 +1,19 @@
 $(document).ready(function () {
+  chrome.storage.sync.get(
+    ["newSelectionPrice", "tabSelectionId"],
+    function (newSelection, tabSelection) {
+      chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+        if (newSelection && !tabs[0].url) {
+          $("#myModal").fadeIn();
+        } else {
+          console.log("Current tab title: " + tabs[0].title);
+          console.log("Current tab URL: " + tabs[0].url);
+          console.log("Current tab: " + tabs[0].id);
+        }
+      });
+    }
+  );
+
   $("p").click(function () {
     $(this).hide();
   });
