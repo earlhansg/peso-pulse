e22367f05f6b5a811f648ecf112cf7be48c6e09e
Earl Hans Genoso <earlhansg@gmail.com>
2023-11-04 09:48:25 +0800

    Introduced the 'updateFundsAndStorage' function, which facilitates the process of setting and adding funds in both local and Chrome storage.

--- js/popup.js
+++ js/popup.js
@@ -4,28 +4,31 @@
   $(document).ready(function () {
     // write code here
 
-    chrome.storage.sync.get(["expensesList", "remainingFunds"], function (data) {
-      console.log("popup data", data);
-      if (data.expensesList.length > 0) {
-        console.log("approved");
-        $("#down-icon").fadeIn();
-        $(".spend-content").fadeIn();
-        data.expensesList.map((expense) => {
-          $("#spendList").append(`<li class="list-group-item">
+    chrome.storage.sync.get(
+      ["expensesList", "remainingFunds"],
+      function (data) {
+        console.log("popup data", data);
+        if (data.expensesList.length > 0) {
+          console.log("approved");
+          $("#down-icon").fadeIn();
+          $(".spend-content").fadeIn();
+          data.expensesList.map((expense) => {
+            $("#spendList").append(`<li class="list-group-item">
             <span class="item-list">
               <span>${expense.name}</span>
               <span>${expense.price}</span>
             </span>
           </li>`);
-          total += parseInt(expense.price);
-          $("#spend").text(`${total}`);
-        });
-      } else {
-        console.log("no data as of this moment");
+            total += parseInt(expense.price);
+            $("#spend").text(`${total}`);
+          });
+        } else {
+          console.log("no data as of this moment");
+        }
+        $("#spend").text(`${total}`);
+        $("#funds").text(`${funds}`);
       }
-      $("#spend").text(`${total}`);
-      $("#funds").text(`${funds}`);
-    });
+    );
 
     chrome.storage.sync.set({
       totalExpenses: total,
@@ -33,6 +36,24 @@
 
     $("#down-icon").click(function () {
       $(".spend-content").fadeToggle();
+    });
+
+    // Define a function to update funds and store data in Chrome storage
+    function updateFundsAndStorage(inputValue, callback) {
+      funds += parseInt(inputValue, 10);
+      $("#funds").text(`${funds}`);
+      chrome.storage.sync.set({ remainingFunds: funds }, callback);
+    }
+
+    // Attach a click event handler to the button
+    $("#budget-btn").click(function () {
+      const inputValue = $("#budget-input").val();
+
+      if (inputValue) {
+        updateFundsAndStorage(inputValue, function () {
+          $("#budget-input").val("");
+        });
+      }
     });
   });
 })();
