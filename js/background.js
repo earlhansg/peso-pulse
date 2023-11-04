111f51ae9fa61ff5abbd27d0bb7ded394d60e9f8
Earl Hans Genoso <earlhansg@gmail.com>
2023-11-04 09:17:00 +0800

    Implemented a notification feature that can verify whether the user possesses sufficient funds to accommodate a list of expenses.

--- js/background.js
+++ js/background.js
@@ -6,21 +6,57 @@ chrome.contextMenus.create({
   contexts: ["selection"],
 });
 
-chrome.storage.sync.set({"expensesList": []});
+chrome.storage.sync.set({ expensesList: [] });
+chrome.storage.sync.set({ remainingFunds: 0 });
+
+let funds = 0;
+async function fetchFunds() {
+  try {
+    const response = await chrome.storage.sync.get("remainingFunds");
+    if (response && response.remainingFunds !== undefined) {
+      console.log(response);
+      funds = response.remainingFunds;
+    } else {
+      throw new Error("Failed to fetch data");
+    }
+  } catch (error) {
+    console.error(error);
+    throw error; // Rethrow the error to be handled by the calling code
+  }
+}
+
+fetchFunds();
 
 let removeCharacter = (selectedString) => {
   return selectedString.replace(/[^0-9]/g, "");
 };
 
 chrome.contextMenus.onClicked.addListener((clickData, tab) => {
-  console.log("clickData", clickData, tab);
+  console.log("clickData", clickData, tab, funds);
   if (clickData.menuItemId === "PesoPulse" && clickData.selectionText) {
-    if (removeCharacter(clickData.selectionText)) {
-      chrome.storage.sync.set({'newSelectionPrice': removeCharacter(clickData.selectionText)})
+    if (
+      removeCharacter(clickData.selectionText) &&
+      funds > removeCharacter(clickData.selectionText)
+    ) {
+      chrome.storage.sync.set({
+        newSelectionPrice: removeCharacter(clickData.selectionText),
+      });
       chrome.tabs.create({ url: "../html/modal.html" }, (tab) => {
         console.log("Created a new tab with ID: " + tab.id);
-        chrome.storage.sync.set({'tabSelectionId': tab.id})
+        chrome.storage.sync.set({ tabSelectionId: tab.id });
       });
+    } else {
+      console.log("insufficient funds");
+      let notifOptions = {
+        type: "basic",
+        iconUrl: "../images/icon32.png",
+        title: "Insufficient Fund",
+        message: "Uh Oh! Looks like youve reached your limit!",
+      };
+      chrome.notifications.create("limitNotif", notifOptions);
+      setTimeout(function () {
+        chrome.notifications.clear("limitNotif", function (data) {});
+      }, 2000);
     }
   }
 });
--- js/popup.js
+++ js/popup.js
@@ -1,9 +1,10 @@
 (function () {
   let total = 0;
+  let funds = 0;
   $(document).ready(function () {
     // write code here
 
-    chrome.storage.sync.get(["expensesList"], function (data) {
+    chrome.storage.sync.get(["expensesList", "remainingFunds"], function (data) {
       console.log("popup data", data);
       if (data.expensesList.length > 0) {
         console.log("approved");
@@ -19,18 +20,18 @@
           total += parseInt(expense.price);
           $("#spend").text(`${total}`);
         });
-      }
-      else { 
-        console.log("no data as of this moment")
+      } else {
+        console.log("no data as of this moment");
       }
       $("#spend").text(`${total}`);
+      $("#funds").text(`${funds}`);
     });
 
     chrome.storage.sync.set({
       totalExpenses: total,
     });
 
-    $("#down-icon").click(function(){
+    $("#down-icon").click(function () {
       $(".spend-content").fadeToggle();
     });
   });
